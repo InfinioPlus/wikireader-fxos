a17c68985f4adf10be2f46c26ec4be3cba362031
Harsh Vardhan <harshvd95@gmail.com>
2015-07-08 22:04:46 +0530

    Adding some JS xd

--- js/project.js
+++ js/project.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
+
+	var $wikiElem = $('#posts');
+	var query = $('#query').val();
 	
-$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?', function(json) {
-            $('#posts').html(json.parse.text['*']);
-            $("#posts").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");});
-            $("#posts").find("a").attr("target", "_blank");
-        });
+ 	var wikiurl = 'http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=';
+    var wikiUrl = wikiurl + query;
+
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text('Could not load wikipedia links');
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: "jsonp",
+        success: function(data){
+            for (var i = 0; i <= data[1].length - 1; i++) {
+                var pageLink = '<li><a href="' + data[3][i] + '">' + data[1][i] + '</a></li>';
+                $wikiElem.append(pageLink);
+            };
+
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
 		
 
 });
