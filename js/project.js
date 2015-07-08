0c76df38ff814924c0d3a3db95a389d00e02b6c4
Harsh Vardhan <harshvd95@gmail.com>
2015-07-08 13:47:20 +0530

    Adding files

--- js/project.js
+++ js/project.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	
-
+$.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?', function(json) {
+            $('#posts').html(json.parse.text['*']);
+            $("#posts").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");});
+            $("#posts").find("a").attr("target", "_blank");
+        });
+		
 
 });
