e3ec7ca35b3d0c6c570ae28b63d41173f229511f
Harsh Vardhan <harshvd95@gmail.com>
2015-07-09 14:49:39 +0530

    Adding enter press search

--- js/project.js
+++ js/project.js
@@ -1,25 +1,32 @@
 $(document).ready(function(){
 
-	var $wikiElem = $('#posts');
-	var query = $('#query').val();
-	
- 	var wikiurl = 'http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=';
-    var wikiUrl = wikiurl + query;
+	$('#go').click(function(){
+		var $wikiElem = $('#posts');
+		var query = $('#query').val();
+		
+	 	var wikiurl = 'http://en.wikipedia.org/w/api.php?format=json&action=opensearch&search=';
+	    var wikiUrl = wikiurl + query;
 
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text('Could not load wikipedia links');
-    }, 8000);
+	    var wikiRequestTimeout = setTimeout(function(){
+	        $wikiElem.text('Could not load wikipedia links');
+	    }, 8000);
 
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        success: function(data){
-            for (var i = 0; i <= data[1].length - 1; i++) {
-                var pageLink = '<li><a href="' + data[3][i] + '">' + data[1][i] + '</a></li>';
-                $wikiElem.append(pageLink);
-            };
+	    $.ajax({
+	        url: wikiUrl,
+	        dataType: "jsonp",
+	        success: function(data){
+	            for (var i = 0; i <= data[1].length - 1; i++) {
+	                var pageLink = '<li><a href="' + data[3][i] + '">' + data[1][i] + '</a></li>';
+	                $wikiElem.append(pageLink);
+	            };
 
-            clearTimeout(wikiRequestTimeout);
+	            clearTimeout(wikiRequestTimeout);
+	        }
+	    });
+	});
+	$('#query').keypress(function(e){
+        if(e.which == 13){//Enter key pressed
+            $('#go').click();//Trigger search button click event
         }
     });
     var start = true;
