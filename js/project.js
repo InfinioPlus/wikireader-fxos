fde8e8551d702dc5521e5d00910fc55640e7f767
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2015-07-11 18:34:20 +0200

    Fixing indentation

--- js/project.js
+++ js/project.js
@@ -1,46 +1,44 @@
 $(document).ready(function(){
 
-	$('#go').click(function(){		
-	 	var wikiurl = 'http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + $('#query').val();
+    $('#go').click(function(){		
+        var wikiurl = 'http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + $('#query').val();
 
-	    $.ajax({
-	        url: wikiurl,
-	        dataType: "jsonp",
-	        success: function(data){
+        $.ajax({
+            url: wikiurl,
+            dataType: "jsonp",
+            success: function(data){
                 $('#search-results').empty();
                 
-	            for (var i = 0; i <= data.query.search.length; i++) {
+                for (var i = 0; i <= data.query.search.length; i++) {
                     var element = '';
                     element += '<div class="row rw1"><div class="col-xs-12 col1 well" id="posts">';
                     element += '<h4>' + data.query.search[i].title +'</h4>';
                     element += '<p>' + data.query.search[i].snippet + '</p>';
                     element += '<button class="btn btn-success">Read More..</button>';
                     element += '</div></div><hr>';
-                    
-	                $('#search-results').append(element);
-	            };
-	        }
-	    });
-	});
-    
-	$('#query').keypress(function(e){
+
+                    $('#search-results').append(element);
+                };
+            }
+        });
+    });
+
+    $('#query').keypress(function(e){
         if(e.which == 13){//Enter key pressed
             $('#go').click();//Trigger search button click event
         }
     });
-    
+
     var start = true;
     // $('#wrapper').hide();
     $('#wrapper').addClass("toggled");
     $('#rem-btn').click(function(){
-    	if(start){
-	    	start = false;
-	    	$("#wrapper").toggleClass("toggled");
-	    }
-	    else
-	    {
-	    	start = true;
-	    	$("#wrapper").toggleClass("toggled");
-	    }
+        if(start){
+            start = false;
+            $("#wrapper").toggleClass("toggled");
+        } else{
+            start = true;
+            $("#wrapper").toggleClass("toggled");
+        }
     });
 });
