117707e5f123103fc29fc0e886d31932173e0a7f
GhostAlgorithm <gomezlopez.jorge96@gmail.com>
2015-07-11 17:10:21 +0200

    Search functionality, part 2

--- js/project.js
+++ js/project.js
@@ -1,24 +1,23 @@
 $(document).ready(function(){
 
-	$('#go').click(function(){
-		var $wikiElem = $('#search-results');
-		var query = $('#query').val();
-		
+	$('#go').click(function(){		
 	 	var wikiurl = 'http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + $('#query').val();
 
 	    $.ajax({
 	        url: wikiurl,
 	        dataType: "jsonp",
 	        success: function(data){
+                $('#search-results').empty();
+                
 	            for (var i = 0; i <= data.query.search.length; i++) {
                     var element = '';
                     element += '<div class="row rw1"><div class="col-xs-12 col1 well" id="posts">';
-                    element += '<h4>' + data.query.search[i].titlte +'</h4>';
+                    element += '<h4>' + data.query.search[i].title +'</h4>';
                     element += '<p>' + data.query.search[i].snippet + '</p>';
                     element += '<button class="btn btn-success">Read More..</button>';
                     element += '</div></div><hr>';
                     
-	                $wikiElem.append(pageLink);
+	                $('#search-results').append(element);
 	            };
 	        }
 	    });
